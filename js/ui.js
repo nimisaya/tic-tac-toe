e1f8d31bd250a893c18b1a27507a7fddefca3a75
Amanda Jarvinen <nimisaya@outlook.com>
2020-11-17 13:10:07 +1100

    Create grid squares dynamically

---
+++ js/ui.js
@@ -0,0 +1,24 @@
+
+
+$(document).ready(function(){
+
+  // Set the board size
+  const size = 3; // 3x3
+
+  // Create game
+  game.setup(size);
+
+  const $grid = $('#grid');
+
+  // Display the gameboard
+  // Add grid squares to HTML based on game size
+  for (let i = 0; i < (Math.pow(size, 2)); i++){
+    let $square = $(`<div class="gridSquare" id="square${[i]}"></div>`);
+    // Add within grid
+    $grid.append($square);
+  }
+
+  // TODO: User clicks on square show piece on Board
+  // TODO: Update gameboard & state
+
+});
