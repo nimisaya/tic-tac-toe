a07ae6dc33e5cb4e54b5b2ddbe6f79f07c6a3215
Amanda Jarvinen <nimisaya@outlook.com>
2020-11-21 01:34:03 +1100

    Refactor computer

---
+++ js/computer.js
@@ -0,0 +1,53 @@
+const computer = {
+  difficulty: {
+    level: null,
+
+    setDifficulty: function(difficulty){
+      switch (difficulty){
+        case 'hard':
+          this.level = 2;
+          break;
+        case 'medium':
+          this.level = 1;
+          break;
+        default:
+          this.level = 0;
+      }
+    }
+  }, // difficulty()
+
+  generateRandomInt: function(min = 0, max = game.rowSize){
+    const randomNumber = (Math.random() * (max - min)) + min;
+    return Math.round(randomNumber);
+  }, // generateRandomInt()
+
+  randomPosition: function(){
+    const randomRow = this.generateRandomInt();
+    const randomColumn = this.generateRandomInt();
+    return {row: randomRow, column: randomColumn};
+  }, // randomPosition()
+
+  semiRandomPosition: function(){
+    // const randomRow;
+    // const randomColumn;
+    // return {row: randomRow, column: randomColumn};
+  }, // semiRandomPosition()
+
+  bestPosition: function(){
+    // const bestRow;
+    // const bestColumn;
+    // return {row: bestRow, column: bestColumn};
+  }, // bestPosition()
+
+  takeTurn: function(rowSize){
+    if (this.difficulty.level === 0){
+      const position = this.randomPosition(rowSize);
+    } else if (this.difficulty.level === 1){
+      // const position = this.semiRandomPosition(rowSize);
+    } else {
+      // const position = this.bestPosition(rowSize);
+    }
+
+    return position;
+  }, // takeTurn()
+}; // computer
